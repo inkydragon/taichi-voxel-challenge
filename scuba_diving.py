5da0973b3f949b8f7744d44002d30e7cae29afe7
woclass <git@wo-class.cn>
2022-05-08 21:22:53 +0800

    清理代码+渐变填充

--- scuba_diving.py
+++ scuba_diving.py
@@ -2,27 +2,16 @@ from scene import Scene
 import taichi as ti
 from taichi.math import *
 
-# ==== Constant Def ====
-# --- Grid Const
-MIN_GRID_IDX = 0
-MAX_GRID_IDX = 128
-HALF_GRID = MAX_GRID_IDX / 2
-
 # --- Material Constant
 CLEAR_MAT = 0
 SOLID_MAT = 1
 LIGHT_MAT = 2
 
-# --- Color Constant
-WHITE = ivec3(255) / 255
-GRAY = ivec3(128) / 255
-RED = ivec3(255, 0, 0) / 255
-
 
 # ---- 场景设置
-scene = Scene(exposure=1)
-scene.set_directional_light((1, 1, 1), 0.1, (1, 1, 1))
-scene.set_background_color((0.3, 0.4, 0.6))
+scene = Scene(voxel_edges=0.06, exposure=1)
+# scene.set_directional_light((1, 1, 1), 0.1, (1, 1, 1))
+# scene.set_background_color((0.3, 0.4, 0.6))
 scene.set_floor(-64, (1, 1, 1))
 
 
@@ -32,20 +21,8 @@ def rgb(r, g, b):
     return vec3(r/255.0, g/255.0, b/255.0)
 
 
-@ti.func
-def gray(g):
-    return rgb(g, g, g)
-
-
-# ---- Vox helper func
-@ti.func  # is in grad [0, 128)^3
-def is_in_grid(p):
-    return any(MIN_GRID_IDX <= p < MAX_GRID_IDX)
-
-
 @ti.func  # Input [0, 128) grid => set vox in [-64, 64)
-def fill(p, mat=SOLID_MAT, col=GRAY):
-    # assert is_in_grid(p)
+def fill(p, mat=SOLID_MAT, col=vec3(0.8)):
     # scene.set_voxel(p - HALF_GRID, mat, col)
     scene.set_voxel(p, mat, col)
 
@@ -55,22 +32,24 @@ def remove(p):
     fill(p, mat=CLEAR_MAT)
 
 
-@ti.func  # 填充两点确定的长方体
-def fill_box(x, y, mat, col):
+@ti.func  # z 轴颜色渐变填充
+def z_grad_fill_box(x, y, mat):
+    col0 = rgb(3, 58, 80)
+    col1 = rgb(57, 166, 202)
+    col_rate = 0.2
+    col2 = vec3(1)
     lo, up = ti.min(x, y), ti.max(x, y)+1
+    _steps = up.z - lo.z
+    # up.z <--- Z ---- lo.z :  col0 (a=1) -- col1 @ col_rate --- col2 (a=0)
     for i, j, k in ti.ndrange((lo.x, up.x), (lo.y, up.y), (lo.z, up.z)):
-        fill(vec3(i, j, k), mat, col)
+        a = (k - lo.z) / _steps
+        if a <= col_rate:  # a in [0, col_rate]
+            alpha = 1.0 - (a/col_rate)
+            fill(vec3(i, j, k), mat, mix(col1, col2, alpha))
+        else:  # a in [col_rate, 1.0]
+            alpha = 1.0 - ((a-col_rate)/(1-col_rate))
+            fill(vec3(i, j, k), mat, mix(col0, col1, alpha))
 
-
-@ti.func  # 填充两点确定的长方体
-def slice_box(x, y):
-    fill_box(x, y, mat=CLEAR_MAT, col=GRAY)
-
-
-@ti.func  # 填充两点确定的长方体外壳，内部留空
-def fill_shell(x, y, mat, col):
-    fill_box(x, y, mat, col)
-    slice_box(x+1, y-1)
 
 @ti.func  # 圆形去除. x: 圆心, r: 半径
 def slice_xy_circle(c, r):
@@ -80,6 +59,7 @@ def slice_xy_circle(c, r):
     for x, y in ti.ndrange((x_lo, x_hi+1), (y_lo, y_hi+1)):
         if (x*x+y*y) <= r*r:
             remove(vec3(x, y, z0))
+
 
 @ti.func  # z 轴圆台（圆锥）去除. (c0, r0) 圆台底面, (c1, r1) 圆台顶部
 def slice_x_cone(c0, r0, c1, r1):
@@ -91,21 +71,13 @@ def slice_x_cone(c0, r0, c1, r1):
         r = mix(r1, r0, a)
         slice_xy_circle(c, r)
 
+
 # ==== Main loop
 @ti.kernel
 def initialize_voxels():
-    mat = SOLID_MAT
-    col = gray(200)
-    fill_shell(ivec3(0), ivec3(127), mat, col)
-    slice_box(ivec3(0, 0, 127), ivec3(127))
-    # fill_box(ivec3(HALF_GRID, 0, HALF_GRID), ivec3(HALF_GRID, 64, HALF_GRID), LIGHT_MAT, RED)
-    for y in ti.ndrange(64):
-        scene.set_voxel(ivec3(0, -y, 0), LIGHT_MAT, RED)
-    slice_xy_circle(vec3(0,0,63), 60)
-    slice_x_cone(vec3(0,0,63), 60, vec3(0,0,-60), 2)
+    z_grad_fill_box(ivec3(-62), ivec3(62), LIGHT_MAT)
+    slice_x_cone(c0=vec3(0, 0, 63), r0=40, c1=vec3(0, 0, -63), r1=10)
 
 
-
-if __name__ == '__main__':
-    initialize_voxels()
-    scene.finish()
+initialize_voxels()
+scene.finish()
